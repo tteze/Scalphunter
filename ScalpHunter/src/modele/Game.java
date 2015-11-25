196ec92d2cda3bd0aa461f08be7f7a490c9f5347
Théophile <Thophile@tt>
2015-11-25 17:07:16 +0100

    mise en place du diagramme de classe

---
+++ ScalpHunter/src/modele/Game.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Théophile
+ */
+public class Game {
+    private Player player1;
+    private Player player2;
+    private boolean current_player;
+    private Board board;
+
+    public Game(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.current_player=false;
+        board=new Board();
+    }
+    
+    private Player isEnd(){
+        //TODO
+        return null;
+    }
+    
+    private Player nextPlayer(){
+        //TODO
+        return null;
+    }
+    
+    private void sommonRound(){
+        //TODO
+    }
+    
+    private ArrayList<MoveAttack> attackRound(){
+        //TODO
+        return null;
+    }
+    
+    private void defenseRound(ArrayList<MoveAttack> attacks){
+        //TODO
+    }
+    
+    public Player play(){
+        //TODO
+        return null;
+    }
+    
+    
+}
---
+++ ScalpHunter/src/modele/Minion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Théophile
+ */
+public class Minion extends Card{
+    private int attack;
+    private int defense;
+
+    public Minion(String name, int cost, int attack, int defense) {
+        super(name, cost);
+        this.attack = attack;
+        this.defense = defense;
+    }
+    
+    public void Fight(Minion opponent){
+        //TODO
+    }
+    
+}
